0b78ca307d18674c573305871c60a2ea3ce980ca
dennis42702 <dennis42702@gmail.com>
2024-02-17 15:49:07 -0500

    verification code file updated

---
+++ boilerdate/backend/verification_code.js
@@ -0,0 +1,54 @@
+require('dotenv').config();
+
+const express = require('express')
+const nodemailer = require('nodemailer');
+const app = express()
+const port = 3002
+
+app.listen(port, () => {
+  console.log(`Is listening at http://localhost:${port}`)
+})
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        type: 'OAuth2',
+        user: process.env.USER_EMAIL,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        refreshToken: process.env.REFRESH_TOKEN,
+    }
+});
+
+async function sendVerificationEmail(email, verificationCode) {
+    try {
+        mailOptions = {
+            from: 'boilerdate@gmail.com',
+            to: email,
+            subject: 'Boilerdate Account Creation Email Verification',
+            text: `Your verification code is: ${verificationCode}`
+          };
+        
+        const info = await transporter.sendMail(mailOptions);
+        console.log('Verification code sent: ', info.response);
+    } catch (error) {
+        console.log('Error occurred while sending code: ', error);
+    }
+}
+
+
+function generateVerificationCode() {
+    const characters = '0123456789';
+    let code = '';
+    for (let i = 0; i < 6; i++) {
+        code += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return code;
+}
+
+// Example usage
+const verificationCode = generateVerificationCode();
+const userEmail = 'youngjunyoo0916@gmail.com';
+
+sendVerificationEmail(userEmail, verificationCode);
+
