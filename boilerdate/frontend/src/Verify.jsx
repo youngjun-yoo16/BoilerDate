cbd73fb881bb272a0ffa19ce8f439451c6f15dc6
22leec <62630011+22leec@users.noreply.github.com>
2024-02-19 06:45:13 +0900

    Update Verify.jsx

--- boilerdate/frontend/src/Verify.jsx
+++ boilerdate/frontend/src/Verify.jsx
@@ -5,29 +5,36 @@ import VerificationInput from "react-verification-input";
 
 function Verify() {
   return (
-    <div class="otp-card">
-      <h1>OTP Verifcation</h1>
-      <p></p>
-      <p>Code has been sent to ****@gmail.com</p>
-      <div>
-        <VerificationInput
+    <div className="otp-body">
+      <div class="otp-card">
+        <h1>OTP Verifcation</h1>
+        <p></p>
+        <p>Code has been sent to ****@gmail.com</p>
+        <div>
+          <VerificationInput
             length={6}
             validChars="0-9"
             inputProps={{ inputMode: "numeric" }}
             classNames={{
-                container: "container",
-                character: "character",
-                characterInactive: "character--inactive",
-                characterSelected: "character--selected",
-                characterFilled: "character--filled",
+              container: "container",
+              character: "character",
+              characterInactive: "character--inactive",
+              characterSelected: "character--selected",
+              characterFilled: "character--filled",
             }}
-        />
+          />
+        </div>
+        <br />
+        <p>
+          Didn't receive the code? <a href="#">Resend</a>
+        </p>
+        <button
+          type="submit"
+          className="btn btn-outline-primary rounded-20 w-25"
+        >
+          Verify
+        </button>
       </div>
-      <br />
-      <p>
-        Didn't receive the code? <a href="#">Resend</a>
-      </p>
-      <button type="submit" className="btn btn-outline-primary rounded-20 w-25">Verify</button>
     </div>
   );
 }
