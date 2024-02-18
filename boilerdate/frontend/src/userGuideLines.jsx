197489fb2ab4e9bd5f7c052928278374d835238e
Jeongbin Lee <jblee1152@gmail.com>
2024-02-18 15:31:33 -0500

    fixed html formatting of rules and guideliens

--- boilerdate/frontend/src/userGuideLines.jsx
+++ boilerdate/frontend/src/userGuideLines.jsx
@@ -25,6 +25,7 @@ function UserConsent() {
   };
 
   // return the page
+  // may change the formatting
   return (
     <div>
       <pre>beginningText </pre>
@@ -33,57 +34,104 @@ function UserConsent() {
           <h1 className="text-center mb-4">Rules and Guidelines</h1>
           <ul>
             <li>
-              <strong>Purdue Personnel Only:</strong> Only Purdue students and
-              staff are allowed to create an account.
+              <strong>Purdue Personnel Only</strong>
+              <ol>
+                <li>
+                  Only Purdue students and staff are allowed to create an
+                  account.
+                </li>
+              </ol>
             </li>
             <li>
-              <strong>Adult Only:</strong> You must be at least 18 or older to
-              use BoilerDate. Minors are not allowed on this application.
+              <strong>Adult Only</strong>
+              <ol>
+                <li>
+                  You must be at least 18 or older to use BoilerDate. Minors are
+                  not allowed on this application.
+                </li>
+              </ol>
             </li>
             <li>
-              <strong>Abide Laws:</strong> We do not tolerate anything illegal
-              such as harassment and human trafficking.
+              <strong>Abide Laws</strong>
+              <ol>
+                <li>
+                  We do not tolerate anything illegal such as harassment and
+                  human trafficking.
+                </li>
+              </ol>
             </li>
             <li>
-              <strong>Respect Boundaries:</strong> Do not make other people feel
-              uncomfortable.
+              <strong>Respect Boundaries</strong>
+              <ol>
+                <li>Do not make other people feel uncomfortable.</li>
+                <li>
+                  Do not post any sexual or violent content on your profile.
+                </li>
+                <li>Treat others the way they want to be treated.</li>
+              </ol>
             </li>
             <li>
-              Do not post any sexual or violent content on your profile. Treat
-              others the way they want to be treated.
+              <strong>No More Than One Account per Person</strong>
+              <ol>
+                <li>
+                  Each Purdue student/staff can only create one account with
+                  their corresponding Purdue email.
+                </li>
+              </ol>
             </li>
             <li>
-              <strong>No More Than One Account per Person:</strong> Each Purdue
-              student/staff can only create one account with their corresponding
-              Purdue email.
+              <strong>Inclusion and diversity</strong>
+              <ol>
+                <li>
+                  People of all genders, ethnicity, religion, sexuality, and
+                  background are welcome.
+                </li>
+              </ol>
             </li>
             <li>
-              <strong>Inclusion and diversity:</strong> People of all genders,
-              ethnicity, religion, sexuality, and background are welcome.
+              <strong>Communicate respectfully</strong>
+              <ol>
+                <li>
+                  Communication is the most important key to meeting and
+                  understanding someone new.
+                </li>
+              </ol>
             </li>
             <li>
-              <strong>Communicate respectfully:</strong> Communication is the
-              most important key to meeting and understanding someone new.
+              <strong>Be Yourself: No Impersonation</strong>
+              <ol>
+                <li>
+                  Don’t post other people’s images or pretend to be someone you
+                  are not.
+                </li>
+              </ol>
             </li>
             <li>
-              <strong>Be Yourself: No Impersonation:</strong> Don’t post other
-              people’s images or pretend to be someone you are not.
+              <strong>Report Incidents</strong>
+              <ol>
+                <li>
+                  If any inappropriate incidents or suspicious activity are
+                  detected, please report to us!
+                </li>
+              </ol>
             </li>
             <li>
-              <strong>Report Incidents:</strong> If any inappropriate incidents
-              or suspicious activity are detected, please report to us!
+              <strong>Provide Feedback</strong>
+              <ol>
+                <li>
+                  Your feedback is crucial to us! Please leave your feedback so
+                  that we can better improve your experience.
+                </li>
+              </ol>
             </li>
             <li>
-              <strong>Provide Feedback:</strong> Your feedback is crucial to us!
-              Please leave your feedback so that we can better improve your
-              experience.
-            </li>
-            <li>
-              <strong>Be Honest:</strong> Don't spread false information.
-            </li>
-            <li>
-              Don’t spam. Don’t manipulate others for your purpose. Don't submit
-              misleading reports.
+              <strong>Be Honest</strong>
+              <ol>
+                <li>Don't spread false information.</li>
+                <li>Don’t spam.</li>
+                <li>Don’t manipulate others for your purpose.</li>
+                <li>Don't submit misleading reports.</li>
+              </ol>
             </li>
           </ul>
         </div>
