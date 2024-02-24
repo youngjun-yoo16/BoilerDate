ae91e2e6da12af439163fafb47100a1c84fad74c
dennis42702 <dennis42702@gmail.com>
2024-02-24 12:34:59 -0500

    updtaed profile interests UI and tested

--- boilerdate/frontend/src/Interests.jsx
+++ boilerdate/frontend/src/Interests.jsx
@@ -4,25 +4,116 @@ import { Link } from "react-router-dom";
 //import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import './InterestsPage.css'; 
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 //is there need to send the entire thing to datbase???
 const interestsData = [
-    "LeetCode",
-   "Hiking",
-  "Cooking",
+  "Travel",
+  "Soccer",
+  "Music",
+  "Outdoors",
+  "Movies",
+  "Snowboarding",
+  "Art Galleries",
+  "YouTube",
+  "Xbox",
+  "Beach",
+  "Weightlifting",
+  "Walking My Dog",
+  "World Peace",
+  "Twitter",
+  "Virtual Reality",
+  "Tango",
+  "Tennis",
+  "Skiing",
+  "Dancing",
+  "Road Trips",
+  "Climate Change",
+  "Football",
+  "Festivals",
+  "Tattoos",
+  "Crossfit",
+  "LGBTQ+ Rights",
+  "K-Pop",
   "Reading",
   "Photography",
-  "Traveling",
-  "Music",
+  "Sports",
+  "Country Music",
+  "Sushi",
+  "Hockey",
+  "Skincare",
+  "Running",
+  "Basketball",
+  "Boxing",
+  "Cars",
+  "Walking",
+  "Robotics",
+  "Instagram",
+  "Foodie Tour",
+  "Shopping",
+  "Clubbing",
+  "Self Care",
+  "Yoga",
+  "Gym",
+  "Skateboarding",
+  "Coffee",
+  "AI",
+  "Poetry",
+  "Singing",
+  "Ice Skating",
   "Gaming",
-  "Fitness",
-  "Art",
-  "Movies",
+  "Pilates",
+  "Ice Cream",
+  "Cheerleading",
+  "Motor Sports",
+  "E-Sports",
+  "Painting",
+  "Bowling",
+  "Surfing",
   "Coding",
-  "Dancing"
+  "Cooking",
+  "Motorcycles",
+  "Art",
+  "Tea",
+  "Wine",
+  "Volunteering",
+  "Vlogging",
+  "Anime",
+  "Ramen",
+  "Volleyball",
+  "League of Legends",
+  "Baseball",
+  "Band",
+  "TikTok",
+  "Comedy",
+  "Blogging",
+  "Netflix",
+  "Horror Movies",
+  "Programming",
+  "Math",
+  "Environment",
+  "Stock Exchange",
+  "Equality",
+  "Animals",
+  "Guitar",
+  "Gospel",
+  "Parties",
+  "Hip Hop",
+  "Indoor Activities",
+  "Literature",
+  "Jogging",
+  "Science",
+  "Korean Food",
+  "NightLife",
+  "Online Shopping",
+  "NBA",
+  "Trying New Things",
+  "Self Development"
 ];
 
 function InterestsPage() {
+  const [notification, setNotification] = useState("");
   const [selectedInterests, setSelectedInterests] = useState([]);
   const navigate = useNavigate(); 
 
@@ -30,7 +121,11 @@ function InterestsPage() {
     if (selectedInterests.includes(interest)) {
       setSelectedInterests(selectedInterests.filter(item => item !== interest));
     } else {
-      setSelectedInterests([...selectedInterests, interest]);
+      if(selectedInterests.length < 5) {
+        setSelectedInterests([...selectedInterests, interest]);
+      } else {
+        toast.info("You can select a maximum of 5 interests.");
+      }
     }
   }
 
@@ -44,7 +139,7 @@ function InterestsPage() {
 
   return (
     <div className="container">
-      <h1 className="header-text">Interests</h1>
+      <h1 className="header-text">Your Interests</h1>
       <form onSubmit={handleSubmit}>
       <div className="selectedInterests-container">
       {selectedInterests.map((interest, index) => (
@@ -68,7 +163,7 @@ function InterestsPage() {
           </div>
         ))}
       </div>
-
+      <ToastContainer />
       <button type="submit" className="btn btn-outline-primary w-100">
               Next
         </button>
