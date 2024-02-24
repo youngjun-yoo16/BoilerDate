7ca8d1bce21c701cdc43b06335ac303e505630c1
Jeongbin Lee <jblee1152@gmail.com>
2024-02-24 16:38:27 -0500

    identified root cause; frontend format issue; been debugging and will continue debugging

--- boilerdate/frontend/src/ProfileSetupUploadPhoto.jsx
+++ boilerdate/frontend/src/ProfileSetupUploadPhoto.jsx
@@ -1,21 +1,84 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useState, useRef } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { Container } from "react-bootstrap";
 import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function UploadPhoto() {
-  const [isUploaded, checkIsUploaded] = useState(false);
+  const [file, setFile] = useState(null);
+  const [fileName, setFileName] = useState("No file");
   const navigate = useNavigate();
+  const inputFileRef = useRef(null);
 
-  // need to handle interaction; need to post to server; save image in aws; save image link in db; modify index.js
+  const triggerFileInput = () => {
+    inputFileRef.current.click();
+  };
 
-  // returned page
+  const handleFileChange = (e) => {
+    const selectedFile = e.target.files[0];
+    if (selectedFile) {
+      setFile(selectedFile);
+      setFileName(selectedFile.name);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!file) {
+      toast.error("Please select a file!");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("image", file);
+
+    try {
+      const response = await axios.post(
+        "http://localhost:3001/uploadPhoto",
+        formData
+      );
+      if (response) {
+        toast.success("Photo uploaded successfully!");
+        navigate("/interests");
+      }
+    } catch (error) {
+      toast.error(`Error: ${error.message}`);
+    }
+  };
+
   return (
     <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">
       <div className="bg-white p-3 rounded w-50">
         <h2>Please upload your photo</h2>
+        <form
+          onSubmit={handleSubmit}
+          className="m-3"
+          encType="multipart/form-data"
+        >
+          <div className="mb-3">
+            <button
+              type="button"
+              className="btn btn-outline-primary"
+              onClick={triggerFileInput}
+            >
+              Choose file
+            </button>
+            <span className="ms-2">{fileName}</span>
+          </div>
+          <input
+            id="fileInput"
+            className="d-none"
+            type="file"
+            name="image"
+            onChange={handleFileChange}
+            ref={inputFileRef}
+          />
+          <button type="submit" className="btn btn-primary">
+            Upload
+          </button>
+        </form>
+        <ToastContainer />
       </div>
     </div>
   );
