961afcaf1e7ed02ba27ab70dae4b4db740dbc985
chaewonlee <cwlee0902@gmail.com>
2024-02-25 14:26:46 -0500

    send email to homepage

--- boilerdate/frontend/src/Home.jsx
+++ boilerdate/frontend/src/Home.jsx
@@ -1,6 +1,11 @@
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 function Home() {
+  const { state } = useLocation();
+  const { email } = state || {};
+  console.log(email);
+
   return (
     <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">
       <div className="bg-white p-3 rounded w-25">
