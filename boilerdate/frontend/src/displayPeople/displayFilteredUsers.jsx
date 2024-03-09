3cfc00b1aa4f533f87b3369c99723b348e5d5168
Jeongbin Lee <jblee1152@gmail.com>
2024-03-09 12:07:00 -0500

    created a file to work on profile card

---
+++ boilerdate/frontend/src/displayPeople/displayFilteredUsers.jsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { ToastContainer, toast } from "react-toastify";
+
+function UserConsent() {
+  // set state to false as default
+  //const [isChecked, setIsChecked] = useState(false);
+  const navigate = useNavigate();
+
+  const { state } = useLocation();
+  const { email, firstName, lastName, gender, dob } = state || {};
+
+  return (
+    <Container className="vh-100 d-flex flex-column justify-content-between">
+      <div>
+        <h1> haha </h1>
+      </div>
+      <ToastContainer />
+    </Container>
+  );
+}
+
+export default UserConsent;
