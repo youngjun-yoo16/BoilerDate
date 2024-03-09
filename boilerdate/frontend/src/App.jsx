bd6c899214890c6ad568d3e3dd643c8a68eafae1
dennis42702 <dennis42702@gmail.com>
2024-03-09 13:28:49 -0500

    basic card profile and buttons ui

--- boilerdate/frontend/src/App.jsx
+++ boilerdate/frontend/src/App.jsx
@@ -39,6 +39,7 @@ import Gender from "./settings/Gender";
 import Birthday from "./settings/Birthday";
 import UpdatePassword from "./login/UpdatePassword";
 import SetupBio from "./profile/Bio";
+import DisplayFilteredUsers from "./displayPeople/displayFilteredUsers";
 
 const App = () => {
   return (
@@ -70,6 +71,7 @@ const App = () => {
           <Route path="/settings/degree" element={<Degree />}></Route>
           <Route path="/settings/height" element={<Height />}></Route>
           <Route path="/settings/personality" element={<Personality />}></Route>
+         
           <Route
             path="/settings/relationship"
             element={<Relationship />}
@@ -92,6 +94,7 @@ const App = () => {
           <Route path="/settings/name" element={<Name />}></Route>
           <Route path="/settings/gender" element={<Gender />}></Route>
           <Route path="/settings/birthday" element={<Birthday />}></Route>
+          <Route path="/matches" element={<DisplayFilteredUsers />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
