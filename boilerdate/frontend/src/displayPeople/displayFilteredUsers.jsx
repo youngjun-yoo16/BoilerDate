198d1307a7c3f5eeeedcbf1d5ebef5f7ebf6398e
dennis42702 <dennis42702@gmail.com>
2024-03-10 15:38:16 -0400

    updated card profile ui and displaying filtered users

--- boilerdate/frontend/src/displayPeople/displayFilteredUsers.jsx
+++ boilerdate/frontend/src/displayPeople/displayFilteredUsers.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef,useMemo } from "react";
+import React, { useState, useEffect, useRef, useMemo } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import { ToastContainer, toast } from "react-toastify";
@@ -9,26 +9,55 @@ import CloseIcon from "@mui/icons-material/Close";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import HandleUserLikesAndDislikes from "./HandleLikesDislikes";
 import ArrowCircleUpIcon from "@mui/icons-material/ArrowCircleUp";
+import axios from "axios";
 
 function DisplayFilteredUsers() {
+  //const { state } = useLocation();
+  //const { email } = state || {};
+    // const navigate = useNavigate();
+  const email = "yoo156@purdue.edu"
   // array of users
   // will be getting from the database later and store it here
+  
+  /*
+  const [check, setCheck] = useState(1);
   const [peoples, setPeople] = useState([
     {
       email: "hi@purdue.edu",
-      name: "hi",
+      firstName: "hi",
       major: "CS",
       age: "21",
-      url: `https://media.gettyimages.com/photos/amazon-ceo-jeff-bezos-founder-of-space-venture-blue-origin-and-owner-picture-id1036094130?k=6&m=1036094130&s=612x612&w=0&h=3tKtZs6_SIXFZ2sdRUB4LjAf_GlfCMekP2Morwkt5EM=`,
+      imageUrl: `https://media.gettyimages.com/photos/amazon-ceo-jeff-bezos-founder-of-space-venture-blue-origin-and-owner-picture-id1036094130?k=6&m=1036094130&s=612x612&w=0&h=3tKtZs6_SIXFZ2sdRUB4LjAf_GlfCMekP2Morwkt5EM=`,
     },
     {
       email: "hello@purdue.edu",
-      name: "hello",
+      firstName: "hello",
       major: "DS",
       age: "20",
-      url: `https://media.gettyimages.com/photos/of-tesla-and-space-x-elon-musk-attends-the-2015-vanity-fair-oscar-picture-id464172224?k=6&m=464172224&s=612x612&w=0&h=M9Wf9-mcTJBLRWKFhAX_QGVAPXogzxyvZeCiIV5O3pw=`,
+      imageUrl: `https://media.gettyimages.com/photos/of-tesla-and-space-x-elon-musk-attends-the-2015-vanity-fair-oscar-picture-id464172224?k=6&m=464172224&s=612x612&w=0&h=M9Wf9-mcTJBLRWKFhAX_QGVAPXogzxyvZeCiIV5O3pw=`,
     },
   ]);
+  */
+
+  const [peoples, setPeople] = useState([]);
+  const [check, setCheck] = useState(0);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.post("http://localhost:3001/fetchFilteredUsers", { email });
+        setPeople(response.data);
+        console.log(response.data);
+        setCheck(1);
+      } catch (error) {
+        toast.error("Failed to fetch profile data");
+        console.error("Error fetching profile:", error);
+      }
+    };
+    fetchData();
+  }, [email]);
+ 
+  console.log(peoples)
+  
 
   const [currentIndex, setCurrentIndex] = useState(peoples.length - 1);
   const [lastDirection, setLastDirecton] = useState();
@@ -40,23 +69,18 @@ function DisplayFilteredUsers() {
       Array(peoples.length)
         .fill(0)
         .map((i) => React.createRef()),
-    []
+    [peoples]
   )
+  console.log(childRefs);
 
   const updateCurrentIndex = (val) => {
     setCurrentIndex(val)
     currentIndexRef.current = val
   }
 
+  const canGoBack = currentIndex < peoples.length - 1
+
   const canSwipe = currentIndex >= 0
-
-  // navigation must be done from other page
-  const { state } = useLocation();
-  const { email } = state || {};
-  // const navigate = useNavigate();
-
-  // console.log(email);
-
 
   const swiped = (direction, nameToDelete, index) => {
     setLastDirecton(direction)
@@ -65,67 +89,75 @@ function DisplayFilteredUsers() {
 
   const outOfFrame = (name, idx) => {
     console.log(`${name} (${idx}) left the screen!`, currentIndexRef.current)
- 
     currentIndexRef.current >= idx && childRefs[idx].current.restoreCard()
 
   }
 
-  const handleSubmit = async (buttonType) => {
-    const temp_email = "lee3546@purdue.edu";
+  const goBack = async () => {
+    if (!canGoBack) return
+    const newIndex = currentIndex + 1
+    updateCurrentIndex(newIndex)
+    await childRefs[newIndex].current.restoreCard()
+  }
+
+  const swipe = async (buttonType) => {
+   // const temp_email = "lee3546@purdue.edu";
     if (buttonType === "like") {
       if (canSwipe && currentIndex < peoples.length) {
         await childRefs[currentIndex].current.swipe('right') // Swipe the card!
       }
       //HandleUserLikesAndDislikes(temp_email, peoples[0].email, true);
-      HandleUserLikesAndDislikes(temp_email, peoples[1].email, true);
+     // HandleUserLikesAndDislikes(temp_email, peoples[1].email, true);
     } else if (buttonType === "dislike") {
       if (canSwipe && currentIndex < peoples.length) {
         await childRefs[currentIndex].current.swipe('left') // Swipe the card!
       }
-      HandleUserLikesAndDislikes(temp_email, peoples[0].email, false);
+     // HandleUserLikesAndDislikes(temp_email, peoples[0].email, false);
       //HandleUserLikesAndDislikes(temp_email, peoples[1].email, false);
-    } else if (buttonType === "arrow") {
+    } 
+  };
 
+  const handleSubmit = (e) => {
+     if (e === "arrow") {
 
     }
   };
 
-  
   let message;
-if (lastDirection === 'left') {
-  message = (
-    <h2 className='infoText'>
-      NOPE
-    </h2>
-  );
-} else if (lastDirection === 'right') {
-  message = (
-    <h2 className='infoText'>
-      LIKE
-    </h2>
-  );
-}
+  if (lastDirection === 'left') {
+    message = (
+      <h2 className='infoText'>
+        NOPE
+      </h2>
+    );
+  } else if (lastDirection === 'right') {
+    message = (
+      <h2 className='infoText'>
+        LIKE
+      </h2>
+    );
+  }
 
-
+  if(check===1) {
   return (
     <div className="tinderCard_container">
       {peoples.map((person, index) => (
         <TinderCard
-          key={person.name}
+          key={person.firstName}
           ref={childRefs[index]}
           className="swipe"
           preventSwipe={[`up`, `down`]}
-          onSwipe={(dir) => swiped(dir, person.name, index)}
-          onCardLeftScreen={() => outOfFrame(person.name, index)}
+          onSwipe={(dir) => swiped(dir, person.firstName, index)}
+          onCardLeftScreen={() => outOfFrame(person.firstName, index)}
         >
           <div
-            className="card"
+            className="card filter"
             style={{
-              backgroundImage: "url(" + person.url + ")",
+              backgroundImage: "url(" + person.imageUrl + ")",
             }}
           >
-            <h3>
-              {person.name} {person.age}{" "}
+            <h3 className="main_text">
+              {person.firstName} {person.age}{" "}
             </h3>
             <div className="arrowButton">
               <IconButton onClick={() => handleSubmit("arrow")}>
@@ -141,14 +173,14 @@ if (lastDirection === 'left') {
       ))}
 
       <div className="swipeButton">
-        <IconButton onClick={() => handleSubmit("dislike")}>
+        <IconButton onClick={() => swipe("dislike")}>
           <CloseIcon
             sx={{ color: "black" }}
             fontSize="large"
             className="close_button"
           />
         </IconButton>
-        <IconButton onClick={() => handleSubmit("like")}>
+        <IconButton onClick={() => swipe("like")}>
           <FavoriteIcon
             sx={{ color: "red" }}
             fontSize="large"
@@ -156,13 +188,16 @@ if (lastDirection === 'left') {
           />
         </IconButton>
       </div>
-      
-      <div className="message"> 
-      {message} 
-       </div>
-            
+
+      <div className="message">
+        {message}
+      </div>
+
     </div>
   );
+          }
 }
 
 export default DisplayFilteredUsers;
+
+
