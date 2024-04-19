5951c322b958804e737e17299571d6688ad15868
youngjun-yoo16 <yoo156@purdue.edu>
2024-04-19 10:11:23 -0400

    Test for continuous deployment

--- boilerdate/frontend/src/home/Home.jsx
+++ boilerdate/frontend/src/home/Home.jsx
@@ -114,7 +114,7 @@ function Home() {
     <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">
       <div className="bg-white p-3 rounded w-25 border border-primary">
         <h2>
-          <FontAwesomeIcon icon={faHome} /> Home
+          <FontAwesomeIcon icon={faHome} /> Deploy Test
         </h2>
         <br />
         <div className="mb-3">
